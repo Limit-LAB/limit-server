63af1024fd0125c94ec8f99c9273ec2b3013b65e
ho-229 <56023510+ho-229@users.noreply.github.com>
2022-11-18 20:11:17 +0800

    add subs service

---
+++ limit-server-subs/src/lib.rs
@@ -0,0 +1,83 @@
+#![feature(type_alias_impl_trait)]
+
+use limit_utils::{execute_background_task, BackgroundTask};
+use tokio_util::sync::ReusableBoxFuture;
+
+use anyhow::Context;
+use chrono::{Duration, Utc};
+use diesel::{ExpressionMethods, QueryDsl, RunQueryDsl};
+use limit_config::GLOBAL_CONFIG;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+use volo_grpc::{Request, Response, Status};
+
+pub use volo_gen::limit::subs::{self, *};
+
+pub struct SubsService;
+
+macro_rules! redis {
+    ($req:ident) => {
+        $req.extensions()
+            .get::<limit_db::RedisClient>()
+            .context("no redis extended to service")
+            .map_err(|e| {
+                tracing::error!("{}", e);
+                Status::internal(e.to_string())
+            })?
+            .clone()
+    };
+}
+
+macro_rules! dbpool {
+    ($req:ident) => {
+        $req.extensions()
+            .get::<limit_db::DBPool>()
+            .context("no db extended to service")
+            .map_err(|e| {
+                tracing::error!("{}", e);
+                Status::internal(e.to_string())
+            })?
+            .clone()
+    };
+}
+
+#[volo::async_trait]
+impl subs::SubsService for SubsService {
+    async fn get_subscribable_objects(
+        &self,
+        req: Request<GetObjectsRequest>,
+    ) -> Result<Response<GetSubscribableObjectsResponse>, Status> {
+        let subs_req = req.get_ref();
+
+        let auth = subs_req.token.as_ref().ok_or_else(|| {
+            tracing::error!("no auth token");
+            Status::unauthenticated("no auth token")
+        })?;
+        let _claim = limit_server_auth::decode_jwt(&auth.jwt)?;
+
+        let redis = redis!(req);
+
+        let pool = dbpool!(req);
+
+        Err(Status::internal("no implementation"))
+    }
+
+    async fn get_profiles(
+        &self,
+        req: Request<GetObjectsRequest>,
+    ) -> Result<Response<GetProfilesResponse>, Status> {
+        let subs_req = req.get_ref();
+
+        let auth = subs_req.token.as_ref().ok_or_else(|| {
+            tracing::error!("no auth token");
+            Status::unauthenticated("no auth token")
+        })?;
+        let _claim = limit_server_auth::decode_jwt(&auth.jwt)?;
+
+        let redis = redis!(req);
+
+        let pool = dbpool!(req);
+
+        Err(Status::internal("no implementation"))
+    }
+}
